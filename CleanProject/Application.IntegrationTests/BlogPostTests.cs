53c014038cea31a8ccf0d92c7b96bd7cc6fe7bff
BeefierCone853 <102178115+BeefierCone853@users.noreply.github.com>
2024-02-21 20:57:34 +0100

    Add integration tests for UpdateBlogPostCommandHandler

--- CleanProject/Application.IntegrationTests/BlogPostTests.cs
+++ CleanProject/Application.IntegrationTests/BlogPostTests.cs
@@ -1,10 +1,12 @@
 ﻿using Application.Exceptions;
 using Application.Features.BlogPosts.Commands.CreateBlogPost;
 using Application.Features.BlogPosts.Commands.DeleteBlogPost;
+using Application.Features.BlogPosts.Commands.UpdateBlogPost;
 using Application.Features.BlogPosts.DTOs;
 using Application.Features.BlogPosts.Queries.GetBlogPostById;
 using Application.IntegrationTests.Abstractions;
 using Domain.Entities;
+using FluentAssertions;
 
 namespace Application.IntegrationTests;
 
@@ -101,5 +103,70 @@ public class BlogPostTests(IntegrationTestWebAppFactory factory) : BaseIntegrati
         // Assert
         var blogPost = DbContext.BlogPosts.FirstOrDefault(blogPost => blogPost.Id == id);
         Assert.Null(blogPost);
+    }
+
+    [Fact]
+    public async Task Update_ShouldUpdate_WhenBlogPostExists()
+    {
+        // Arrange
+        const int id = 1;
+        var blogPostDto = new CreateBlogPostDto("Title", "Description");
+        var createCommand = new CreateBlogPostCommand(blogPostDto);
+        await Sender.Send(createCommand);
+        var updateBlogPostDto = new UpdateBlogPostDto("New title", "New description");
+        var updateCommand = new UpdateBlogPostCommand(updateBlogPostDto, id);
+        var existingBlogPost = DbContext.BlogPosts.FirstOrDefault(blogPost => blogPost.Id == id);
+        Assert.NotNull(existingBlogPost);
+        
+        // Act
+        await Sender.Send(updateCommand);
+        
+        // Assert
+        var blogPost = DbContext.BlogPosts.FirstOrDefault(blogPost => blogPost.Id == id);
+        Assert.NotNull(blogPost);
+        blogPost.Title.Should().Be("New title");
+        blogPost.Description.Should().Be("New description");
+    }
+    
+    [Fact]
+    public async Task Update_ShouldThrowCustomValidationException_WhenTitleIsEmpty()
+    {
+        // Arrange
+        var blogPostDto = new UpdateBlogPostDto("", "Description");
+        var command = new UpdateBlogPostCommand(blogPostDto, 1);
+
+        // Act
+        Task Action() => Sender.Send(command);
+
+        // Assert
+        await Assert.ThrowsAsync<CustomValidationException>(Action);
+    }
+
+    [Fact]
+    public async Task Update_ShouldThrowCustomValidationException_WhenDescriptionIsEmpty()
+    {
+        // Arrange
+        var blogPostDto = new UpdateBlogPostDto("Title", "");
+        var command = new UpdateBlogPostCommand(blogPostDto, 1);
+
+        // Act
+        Task Action() => Sender.Send(command);
+
+        // Assert
+        await Assert.ThrowsAsync<CustomValidationException>(Action);
+    }
+
+    [Fact]
+    public async Task Update_ShouldThrowCustomValidationException_WhenTitleAndDescriptionAreEmpty()
+    {
+        // Arrange
+        var blogPostDto = new UpdateBlogPostDto("", "");
+        var command = new UpdateBlogPostCommand(blogPostDto, 1);
+
+        // Act
+        Task Action() => Sender.Send(command);
+
+        // Assert
+        await Assert.ThrowsAsync<CustomValidationException>(Action);
     }
 }
